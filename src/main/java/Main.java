1842ea5c7a5a29f1cebf32343ec8e78bada2bfee
MaksBujanov <55047071+MaksBujanov@users.noreply.github.com>
2022-11-27 23:01:40 +0700

    Main Ver1. Все подряд.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,44 @@
+import java.util.Scanner; // Добавили ввод с клавиатуры
 public class Main {
-
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир2");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите на скольких человек необходимо разделить счёт");
+        while (true) { // Запускаем бесконечный цикл
+            int friends = scanner.nextInt(); //Получаем кол-во гостей от пользователя
+
+            if (friends > 1) {
+                System.out.println(friends + " Гостей");
+                break;
+            }
+            else if (friends==1) {
+                System.out.println("нет смысла ничего считать и делить. Введите значение заново");
+            }
+            else  {
+                System.out.println("Это некорректное значение для подсчёта, количество гостей должно быть >1 . Введите значение ");
+            }
+        }
+        Calc calculator = new Calc(friends); //
+
+        while (true){
+            System.out.println("Введите название товара");
+            String productName = scanner.next();
+
+            System.out.println("Введите стоимость товара. Она должна быть в формате рубли.копейки, например 10.45 или 11.40.");
+            double stoimost = scanner.nextDouble();//Double т.к. формат рубли.копейки
+
+            calc.addItem(new Item(productName, stoimost)); //добавим товар.
+
+            System.out.println("Товар успешно добавлен");
+            System.out.println("Добавить ещё один товар? Введите 'Завершить' если больше нет товаров для добавления");
+
+            String question = scanner.next();
+            String answer = new String ("Завершить");;
+            if (question == answer) {
+                break;
+            }
+        }
     }
 }
